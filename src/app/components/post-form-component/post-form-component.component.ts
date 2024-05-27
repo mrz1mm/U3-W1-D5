fb9c0b9ffa5672db132572746a882f5f6717feac
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2024-05-27 16:37:04 +0200

    Added

---
+++ src/app/components/post-form-component/post-form-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-form-component',
+    templateUrl: './post-form-component.component.html',
+    styleUrls: ['./post-form-component.component.scss'],
+})
+export class PostFormComponentComponent {}
